629b1ddb641eacfd184efbf1693469181bb2e4f2
mandarGG <m.g.g@live.com>
2018-03-26 17:17:56 +0530

    Adding User signup logic

--- backend/movie_recommender/booking_system/views.py
+++ backend/movie_recommender/booking_system/views.py
@@ -1,5 +1,8 @@
 from rest_framework import generics
 from .serializers import CastSerializer, Cast, MovieSerializer, Movie
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -24,4 +27,19 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
 
