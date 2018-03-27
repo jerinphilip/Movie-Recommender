a92aa88dcbc6a8f15a142b243575be8bb1d669ef
Harshil Goel <harshil158@gmail.com>
2018-03-27 17:35:54 +0530

    first commit

--- backend/movie_recommender/functors/notifier.py
+++ backend/movie_recommender/functors/notifier.py
@@ -1,10 +1,27 @@
+from django.core.mail import send_mail
+
 
 class Notifier:
     def __init__(self, config):
         pass
 
-    def mail(User, body):
-        pass
+    @staticmethod
+    def mail(user, subject, body):
+        """
+        Usage: Notifier.mail(args)
+        :param user: User model(s) to send the email to
+        :param subject: Subject of the mail
+        :param body: Body of the mail
+        :return:
+        """
+        return send_mail(
+            subject,
+            body,
+            "no-reply@booker.com",
+            [user.email if not isinstance(user, list) else [u.email for u in user]],
+            fail_silently=False,
+        )
 
-    def sms(User, body):
-        pass
+    @staticmethod
+    def sms(user, body):
+        return True
