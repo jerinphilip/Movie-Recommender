b45d243f1dc21cc86721e48b8f37a05169057a22
Jerin Philip <jerin.philip@research.iiit.ac.in>
2018-03-29 15:29:21 +0530

    Updating with whoosh sample code

--- movie_recommender/booking_system/search_indexes.py
+++ movie_recommender/booking_system/search_indexes.py
@@ -4,10 +4,14 @@ from .models import Movie,Genre,Language
 class MovieIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
     title = indexes.CharField(model_attr='title')
+    id = indexes.CharField(model_attr='id')
     synopsis = indexes.CharField(model_attr='synopsis')
     # language = indexes.CharField(model_attr='language')
     # crew = indexes.CharField(model_attr='crew')
     # genres = indexes.CharField(model_attr='genres')
+
+    def page(self):
+        return '/movie/{}'.format(self.id)
   
     def get_model(self):
       return Movie
