8fb7ed183f1f9584e77daffef5213eb00d3cbd06
Jerin Philip <jerin.philip@research.iiit.ac.in>
2018-03-29 15:49:27 +0530

    More changes

--- movie_recommender/booking_system/search_indexes.py
+++ movie_recommender/booking_system/search_indexes.py
@@ -1,24 +1,21 @@
 from haystack import indexes
 from .models import Movie,Genre,Language
- 
-class MovieIndex(indexes.SearchIndex, indexes.Indexable):
-    text = indexes.CharField(document=True, use_template=True)
-    title = indexes.CharField(model_attr='title')
-    id = indexes.CharField(model_attr='id')
-    synopsis = indexes.CharField(model_attr='synopsis')
-    # language = indexes.CharField(model_attr='language')
-    # crew = indexes.CharField(model_attr='crew')
-    # genres = indexes.CharField(model_attr='genres')
 
-    def page(self):
-        return '/movie/{}'.format(self.id)
-  
+class MovieIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.EdgeNgramField(document=True, use_template=True)
+    title = indexes.EdgeNgramField(model_attr='title')
+    id = indexes.EdgeNgramField(model_attr='id')
+    synopsis = indexes.EdgeNgramField(model_attr='synopsis')
+    # language = indexes.EdgeNgramField(model_attr='language')
+    # crew = indexes.EdgeNgramField(model_attr='crew')
+    # genres = indexes.EdgeNgramField(model_attr='genres')
     def get_model(self):
       return Movie
 
 class GenreIndex(indexes.SearchIndex, indexes.Indexable):
-    text = indexes.CharField(document=True, use_template=True)
-    genre = indexes.CharField(model_attr='genre')
-  
+    text = indexes.EdgeNgramField(document=True, use_template=True)
+    title = indexes.EdgeNgramField(model_attr='genre')
+    id = indexes.EdgeNgramField(model_attr='id')
+
     def get_model(self):
         return Genre
